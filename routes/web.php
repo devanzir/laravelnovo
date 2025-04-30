a4cc786f79e1ee2300df432b1b08736928f18ec6
Junior Fritz <123272630+devanzir@users.noreply.github.com>
2025-04-30 20:38:40 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-#use Illuminate\Routing\Route;
+
 use Illuminate\Support\Facades\Auth;
 use PhpParser\Node\Stmt\Return_;
 use Illuminate\Support\Facades\Route;
@@ -11,8 +11,6 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/product/{slug}', 'HomeController@single')->name('product.single');
 
 Route::post('/payment/process', [PaymentController::class, 'process'])->name('payment.process');
-
-#Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 
 Route::prefix('cart')->name('cart.')->group(function(){
     Route::get('/', 'CartController@index')->name('index');
@@ -41,8 +39,6 @@ Route::group(['middleware' => ['auth']], function(){
 
         Route::post('photos/remove','ProductPhotoController@removePhoto')->name('photo.remove');
 
-        #Route::resource('stores', 'Admin\StoreController');  
-
     });
 });
 Auth::routes();  
@@ -65,102 +61,3 @@ Route::get('/model', function () { });
 
 
 
-// Route::prefix('stores')->name('stores.')->group(function () {
-
-    //     Route::get('/', 'StoreController@index')->name('index');
-
-    //     Route::get('/create', 'StoreController@create')->name('create');
-
-    //     Route::post('/store', 'StoreController@store')->name('store');
-
-    //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-
-    //     Route::post('/update/{store}', 'StoreController@update')->name('update');
-
-    //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
-    // });
-
-
-
-
-
-#Route::get('/home', 'HomeController@index')->name('home');
-
-
-
-
-
-
-
-    #$products = \App\Product::all();
-    #return $products;
-
-    #$user = new \App\User(); 
-    #$user->name = 'Usuario Teste';
-    #$user->email = 'email30@teste.com';
-    #$user->password = bcrypt('123456789');
-
-    #$user = \App\User::create([
-    #'name' => 'junior fritz',
-    #'email' => 'teste@email,com',
-    # 'password' => bcrypt('1234ghd')
-    # ]);
-
-    #dd($user);
-    # $user = \App\User::find(48);
-    # $user ->update([
-    #    'name' => 'Atualizando com Mass Update'
-    # ]);
-    #dd($user);
-
-    #como pegar so uma loja de um usuario.
-
-    # $user =\App\User::find(4);
-    #return $user->store;
-
-    // $user = \App\User::find(10);
-    // $store = $user->store()->create([
-    //     'name' =>'loja teste',
-    //     'description' => 'Loja teste de produtos de informatica',
-    //     'mobile_phone' => 'XXXX-XXXX-XX',
-    //     'phone'=> 'XXX-XXX-XX',
-    //     'slug' => 'Loja-teste'
-
-    // ]);
-
-    // dd($store);
-
-    // $store = \App\Store::find(41);
-    // $product = $store->products()->create([
-    //     'name' => 'not DELL',
-    //     'description' => 'core i 5',
-    //     'body' => 'Qualquer coisa',
-    //     'price' => 2999.90,
-    //     'slug' => 'noot-dell',
-    // ]);
-
-    // dd($product);
-
-    //     \App\Category::create([
-    //         'name' => 'Games',
-    //         'description' => null,
-    //         'slug' => 'games'
-    //     ]);
-
-    //     \App\Category::create([
-    //         'name' => 'Nootboock',
-    //         'description' => null,
-    //         'slug' => 'nootboock'
-    //     ]);
-
-    //    return\App\Category::all();
-    // $product =\App\Product::find(41);
-
-    // dd($product->categories()->sync([2]));
-
-
-
-    // return \App\User::all();
-
-
-    // });
